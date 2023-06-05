08112bbd225a524d5d270f803877efe433abb5f9
Sanja Andjelkovic <ssanjass@hotmail.com>
2023-06-05 22:44:16 +0200

    Mademoiselle projec done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,90 @@
 $(document).ready(function(){
     
+    if($('.team-members-slider').length > 0){
+        $(".team-members-slider").owlCarousel({
+            loop: true,
+            autoplay: true,
+            dots: true,
+            items: 1
+            
+        });
+    }
+    
+    //animation
+    //ANIMATION
+    function animation() {
+        let  windowHeight = $(window).height();
+        let scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animationName = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+
+            if (position < windowHeight + scroll - 100) {
+                $(this).addClass(animationName);
+                $(this).css('animation-delay', delay);
+            }
+        });
+    }
+
+
+    $(window).scroll(function () {
+        animation();
+    });
+    animation();
     
     
     
-})
+    
+    //form validation
+    if ($('.contact-form').length > 0) {
+
+        $('.contact-form').validate({
+
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: 'The Name* field is required'
+                },
+                email: {
+                    required: 'The Email* field is required',
+                    email: 'Please provide a valid email address'
+                },
+                message: {
+                    required: 'The Message* field is required'
+                }
+            },
+
+            errorElement: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest(".form-group").find(".error-msg"));
+            }
+
+
+
+
+        });
+
+    }
+  
+  
+    
+});
 
